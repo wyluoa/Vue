7a45aa3d25216da153e38dbb41078632a65788ea
Wenyi Luo <wenyi@WenyideMacBook-Pro.local>
2021-01-12 21:06:39 +0800

    drm.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -36,7 +36,12 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data () {
+    return {
+      
+    }
+  },
 }
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  }
 }
